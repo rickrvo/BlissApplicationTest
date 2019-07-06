2b32d217749b68ee3121cfffdad77716f6b1c569
Henrique Ormonde <rickrvo@hotmail.com>
2019-07-06 17:40:23 -0300

    New Project creation

---
+++ BlissApplicationsTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BlissApplicationsTest
+//
+//  Created by Henrique Ormonde on 06/07/19.
+//  Copyright © 2019 Rick. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
