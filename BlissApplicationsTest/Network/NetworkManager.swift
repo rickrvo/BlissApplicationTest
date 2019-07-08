523a33cfbfa3971f02d335116630d4950370ac14
Henrique Ormonde <rickrvo@hotmail.com>
2019-07-08 00:54:00 -0300

    Added NetworkManager and initial views

---
+++ BlissApplicationsTest/Network/NetworkManager.swift
@@ -0,0 +1,121 @@
+//
+//  NetworkManager.swift
+//  BlissApplicationsTest
+//
+//  Created by Henrique Ormonde on 07/07/19.
+//  Copyright © 2019 Rick. All rights reserved.
+//
+
+import Foundation
+
+class NetworkManager {
+    
+    var questions : [QuestionModel]
+    var genresList : [Int : [QuestionModel]]
+    
+    var currentQuestionListOffset : Int
+    
+    var totalGenreListPage : [Int : Int]
+    var totalTopRatedPage : Int
+
+    
+    private static var sharedNetworkManager: NetworkManager = {
+        let questionManager = NetworkManager()
+        return questionManager
+    }()
+    
+    let healthService = HealthNetworkService()
+    let questionService = QuestionNetworkService()
+    let shareService = ShareNetworkService()
+    
+    private init() {
+        self.questions = []
+        self.genresList = [:]
+        
+        self.currentQuestionListOffset = 0
+    }
+    
+    // Accessors
+    class func shared() -> NetworkManager {
+        return sharedNetworkManager
+    }
+    
+    // MARK: - Health Check
+    
+    func checkService(completion: ((Bool) ->())?) {
+        
+        healthService.getHealthState { (result, data) in
+            
+            if result.code != 200 || data == nil {
+                completion?(false)
+                return
+            }
+            print("checking")
+            if data?.status?.uppercased() == "OK" {
+                completion?(true)
+            } else {
+                completion?(false)
+            }
+        }
+        
+    }
+    
+    // MARK: - Questions
+    
+    func getQuestion(id: Int) -> QuestionModel? {
+        
+        if self.questions.count > 0 {
+            let index = self.questions.firstIndex { (question) -> Bool in
+                question.id == id
+            }
+            if index != nil {
+                return self.questions[index!]
+            }
+        }
+        return nil
+    }
+
+    
+    func getQuestionList(completion: ((Bool)->())?) {
+        questionService.getQuestionList(limit: 10, offset: self.currentQuestionListOffset, filter: nil) { [unowned self] (result, data) in
+            if result.code != 200 || data == nil {
+                completion?(false)
+                return
+            }
+            print("reading questions")
+            if let genre = data{
+                genre.forEach{
+                    print($0.id ?? "")
+                    let g0 = $0
+                    if (!self.questions.contains(where: { (g) -> Bool in
+                        g.id == g0.id
+                    })) {
+                        self.questions.append(g0)
+                    }
+                }
+            }
+            self.currentQuestionListOffset = self.questions.last?.id ?? self.currentQuestionListOffset + 10
+            completion?(true)
+        }
+    }
+    
+
+    // MARK: - Share
+    
+    func share(email: String, content: String, completion: ((Bool)->())?) {
+        
+        shareService.shareQuestion(destinationEmail: email, contentUrl: content) { (result, data)  in
+            if result.code != 200 || data == nil {
+                completion?(false)
+                return
+            }
+            print("sharing")
+            if data?.status?.uppercased() == "OK" {
+                completion?(true)
+            } else {
+                completion?(false)
+            }
+        }
+        
+    }
+}
